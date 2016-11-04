d8d71480683ddbe25f9a325b85984047d51abe08
MarkNader <marknader@hotmail.com>
2016-11-04 09:56:09 +0200

    Commenting each line in Cache Class

--- src/memory/Cache.java
+++ src/memory/Cache.java
@@ -43,35 +43,35 @@ public class Cache implements Accessible{
 
 	@Override
 	public Object read(short address, boolean firstLevel) {
-		workCycles += cycleAccessTime;
+		workCycles += cycleAccessTime; //since every Time we read (either miss or hit) , we increase the workCycles
 		short[] result;
 		if(associativity == 1)
-			result = directMappedRead(address);
+			result = directMappedRead(address);  // associativity = 1  --> Direct Mapped
 		else if(associativity == (size / blockSize))
-			result = fullyAssociativeRead(address);
-		else result = setAssociativeRead(address);
+			result = fullyAssociativeRead(address); // associativity = C --> Fully Associative
+		else result = setAssociativeRead(address); // Otherwise --> Set Associative (We assume CPU handles errors .. e.g :- associativity > C)
 		
-		int offset = address % blockSize;
+		int offset = address % blockSize;  
 		if(firstLevel)
-			return result[offset];
+			return result[offset]; // Note that :- first level of Cache , always return a certain word , while all other levels (including memory) returns the full block
 		return result;
 	}
 
 	private short[] setAssociativeRead(short address) {
-		short addressCopy = address;
-		address=(short) (address/blockSize);
-		short noLines = (short) (size / blockSize);
-		short noSets = (short)(noLines / associativity);
+		short addressCopy = address; //make a copy of address , because the original one will be manipulated
+		address=(short) (address/blockSize); //getting rid of offset
+		short noLines = (short) (size / blockSize);  //calculating number of lines
+		short noSets = (short)(noLines / associativity); // calculating number of sets
 		int index = address % noSets;
 		int tag = address / noSets;
-		int emptySpaceIndex = -1;
-		boolean found = false;
-		int start = index * associativity;
+		int emptySpaceIndex = -1; //searching for empty space , initially -1 telling we didn't find one yet
+		boolean found = false;    // This boolean is used to return the first empty space , in order to place content into it (if needed) .. we wont need it for sure if we found our entry later
+		int start = index * associativity; //in order to reach the first entry in our desired set , we should skip sets from 0 to index-1  , those have a count of {index} , each with {associativity} entries , so we skip {index*associativity} entries
 		
 		for(int i=start;i<start + associativity;i++){
 			if(!lines[i].valid && !found){
-				emptySpaceIndex = i;
-				found =true;
+				emptySpaceIndex = i; //whenever finding empty space , record it
+				found =true;         // set boolean to true to prevent future overrides of the emptySpaceIndex variable
 			}
 				
 			if(lines[i].valid && lines[i].tag == tag){
@@ -84,18 +84,18 @@ public class Cache implements Accessible{
 		misses++;
 		
 		if(emptySpaceIndex == -1){
-			Random rand = new Random();
-			int replacedIndex = start + rand.nextInt(associativity);
+			Random rand = new Random(); //if we found no empty space , pick a random entry to remove within the set
+			int replacedIndex = start + rand.nextInt(associativity); // we add {start} to ensure its within our set
 			CacheEntry entry = lines[replacedIndex];
 			
 			if(!writePolicy && entry.valid && entry.dirty){
-				short constuctedAddress = (short) ((entry.tag * noSets + index) * blockSize);
-				this.lowerLevel.write(constuctedAddress, entry.data, false);
+				short constuctedAddress = (short) ((entry.tag * noSets + index) * blockSize); //in case we use the write policy and the entry to be replaced is valid and dirty , this entry must be written into lowerLevel in its specified address, which we re-construct using its tag
+				this.lowerLevel.write(constuctedAddress, entry.data, false); // we send false to ensure that the replaced entry will be written as entire block (surely not firstLevel)
 			}
-			index = replacedIndex;
+			index = replacedIndex; //we update our index now with the replaced index . In case we didn't enter this if statement , the index would have remained the empty entry we found upwards (before the if statement)
 		}
 			
-		lines[index] = new CacheEntry((short)tag, (short[]) this.lowerLevel.read(addressCopy, false));
+		lines[index] = new CacheEntry((short)tag, (short[]) this.lowerLevel.read(addressCopy, false)); // we search in lowerLevel , and initialize our data to whatever resulting from the read in that lowerLevel 
 		lines[index].valid = true;
 		return lines[index].data;
 	}
@@ -104,7 +104,7 @@ public class Cache implements Accessible{
 		short tag = (short) (address/blockSize);
 		int emptySpaceIndex = -1;
 		boolean found = false;
-		for(int i=0;i<lines.length;i++){
+		for(int i=0;i<lines.length;i++){   // search the entire cache instead of just the set
 			if(!lines[i].valid && !found){
 				emptySpaceIndex = i;
 				found =true;
@@ -119,13 +119,13 @@ public class Cache implements Accessible{
 		int index = emptySpaceIndex;
 		
 		if(emptySpaceIndex == -1){
-			Random rand = new Random();
-			index = rand.nextInt(size/blockSize);
+			Random rand = new Random();  
+			index = rand.nextInt(size/blockSize); // the random will be any number of the whole cache 
 			
 			CacheEntry entry = lines[index];
 			
 			if(!writePolicy && entry.valid && entry.dirty){
-				short constuctedAddress = (short) (entry.tag * blockSize);
+				short constuctedAddress = (short) (entry.tag * blockSize); //the only difference is the way of constructing the address
 				this.lowerLevel.write(constuctedAddress, entry.data, false);
 			}
 		}
@@ -151,11 +151,11 @@ public class Cache implements Accessible{
 		misses++;
 		
 		if(!writePolicy && entry.valid && entry.dirty){
-			short constuctedAddress = (short) ((entry.tag * noLines +index ) * blockSize);
+			short constuctedAddress = (short) ((entry.tag * noLines +index ) * blockSize); //different way of constructing the address
 			this.lowerLevel.write(constuctedAddress, entry.data, false);
 		}
 			
-			
+		//no replacement policy --> no randomness --> always we know what to replace because it's a single option
 		lines[index] = new CacheEntry((short)tag, (short[]) this.lowerLevel.read(addressCopy, false));
 		lines[index].valid = true;
 		return lines[index].data;
